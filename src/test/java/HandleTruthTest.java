bcb8c9c4cb3a09d92557b48f8aaf39e5723a9c6c
Martin Matsudaira <mmats23@uw.edu>
2020-11-21 01:33:57 -0800

    Quick test outline. New test case added.

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -7,23 +7,44 @@ import static org.junit.Assert.assertEquals;
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
-        HandleTruth.wordCount(getTestCase(1));
+        HandleTruth.wordCount(getTestCase(2));
     }
 
     @Test
     //test if the words are counted correctly
     public void testCount(){
-        String testCase = getTestCase(0);
-        int[] expected = getExpectedCount(0);
-        HashMap<String, Integer> actual = HandleTruth.wordCount(testCase);
-        String[] words = testCase.split(" ");
+        for(int j = 0; j <= 2; j++) {
+            String testCase = getTestCase(j);
+            int[] expected = getExpectedCount(j);
+            HashMap<String, Integer> actual = HandleTruth.wordCount(testCase);
+            String[] words = testCase.split(" ");
 
-        int i = 0;
-        for(String word : words) {
-            int count = actual.get(word);
-            assertEquals(expected[i], count);
-            i++;
+            int i = 0;
+            for (String word : words) {
+                int count = actual.get(word);
+                assertEquals(expected[i], count);
+                i++;
+            }
         }
+    }
+
+    @Test
+    public void test2(){
+        //code
+    }
+
+    @Test
+    public void test3(){
+        //code
+    }
+    @Test
+    public void test4(){
+        //code
+    }
+
+    @Test
+    public void test5(){
+        //code
     }
 
     public static String getTestCase(int caseNum){
@@ -36,6 +57,11 @@ public class HandleTruthTest {
             case 1:
                 testCase = "once twice twice thrice thrice thrice quads quads quads quads";
                 //each word occurring different times
+                break;
+            case 2:
+                testCase = "are, are. are these, these. these the, the. the same, same. same";
+                //each word followed by some punctuation. Same words, different punctuation, different key values
+                break;
             default:
                 break;
         }
@@ -56,6 +82,12 @@ public class HandleTruthTest {
                                         5, 5, 5, 5, 5,
                                         5, 5, 5, 5, 5,
                                         5, 5, 5, 5, 5};
+                break;
+            case 1:
+                expected = new int[]{1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+                break;
+            case 2:
+                expected = new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
                 break;
             default:
                 break;
